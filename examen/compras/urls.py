bab25e4f7951d1f071ac28ac077ef9569a4a1c72
diegobaeza <kweaculiao@hotmail.com>
2018-12-01 00:02:00 -0300

    Primer commit

---
+++ examen/compras/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from django.conf.urls import include, url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+]
