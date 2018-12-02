03c102addb87ad63f61f4f0ee45e8d4e26e972d6
Diego, Pablo y Nicolas <pablo.ignacio288@gmail.com>
2018-12-02 17:45:58 -0300

    examen

--- examen/compras/models.py
+++ examen/compras/models.py
@@ -3,23 +3,25 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.urls import reverse
 # Create your models here.
-class Productos(models.Model):
-    nombre = models.CharField(max_length=70)
-    costo_presupuesto = models.IntegerField(blank=True)
-    costo_real = models.IntegerField(blank=True)
-    tienda = models.ManyToManyField('Tienda')
-    notas = models.TextField(max_length=200, blank=True)
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=80)
+
+class Region(models.Model):
+    nombre = models.CharField(max_length=80)
 
 class Tienda(models.Model):
     nombre = models.CharField(max_length=80)
     sucursal = models.CharField(max_length=80)
     direccion = models.CharField(max_length=80)
-    ciudad = models.ManyToManyField('Ciudad', max_length=80)
-    region = models.ManyToManyField('Region', max_length=80)
+    ciudad = models.ManyToManyField(Ciudad, max_length=80)
+    region = models.ManyToManyField(Region, max_length=80)
 
-class Region(models.Model):
-    nombre = models.CharField(max_length=80)
+class Productos(models.Model):
+    nombre = models.CharField(max_length=70)
+    costo_presupuesto = models.IntegerField(blank=True)
+    costo_real = models.IntegerField(blank=True)
+    tienda = models.ManyToManyField(Tienda)
+    notas = models.TextField(max_length=200, blank=True)
 
-class Ciudad(models.Model):
-    nombre = models.CharField(max_length=80)
+
     
