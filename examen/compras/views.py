6d9d4edc18ece89472243d3b0eded6f79c360ac6
Diego, Pablo y Nicolas <pablo.ignacio288@gmail.com>
2018-12-02 17:05:09 -0300

    examen

--- examen/compras/views.py
+++ examen/compras/views.py
@@ -3,9 +3,10 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, get_object_or_404
+from .models import Productos, Tienda
 # Create your views here.
-
 
 def index(request):
     return render(request, 'compras/inicio.html', {})
+
 
