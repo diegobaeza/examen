1476faaf5c2685a4038834fe29fb3db6f81c35a5
Diego, Pablo y Nicolas <pablo.ignacio288@gmail.com>
2018-12-02 17:38:41 -0300

    examen

--- examen/compras/models.py
+++ examen/compras/models.py
@@ -7,15 +7,15 @@ class Productos(models.Model):
     nombre = models.CharField(max_length=70)
     costo_presupuesto = models.IntegerField(blank=True)
     costo_real = models.IntegerField(blank=True)
-    tienda = models.ManyToManyField(Tienda)
+    tienda = models.ManyToManyField('Tienda')
     notas = models.TextField(max_length=200, blank=True)
 
 class Tienda(models.Model):
     nombre = models.CharField(max_length=80)
     sucursal = models.CharField(max_length=80)
     direccion = models.CharField(max_length=80)
-    ciudad = models.ManyToManyField(Ciudad, max_length=80)
-    region = models.ManyToManyField(Region, max_length=80)
+    ciudad = models.ManyToManyField('Ciudad', max_length=80)
+    region = models.ManyToManyField('Region', max_length=80)
 
 class Region(models.Model):
     nombre = models.CharField(max_length=80)
